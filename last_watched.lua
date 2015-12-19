8f541eff476e34b39a7fc8d4553f631e43fd9411
banana <bananarangang@gmail.com>
2015-12-19 03:51:49 -0500

    Fixed the issue of changing to a new ep setting the last watched ep to 0%, ugly fix though

--- last_watched.lua
+++ last_watched.lua
@@ -4,6 +4,8 @@ DATASTORE_FILENAME = ".LAST_WATCHED_MPV.MPVWTC"
 datastore_input = persistence.load(DATASTORE_FILENAME) or {}
 global_dir =  "./"
 global_filename = ""
+global_current_number = ""
+global_episode_name = ""
 
 function simple_print_table(table)
    for key,value in pairs(table) do
@@ -20,11 +22,8 @@ function write_data_file(table,path,filename)
 end
 
 function load_file_handler (event)
-   print("File loaded!!!!!")
-
    local path = mp.get_property("path", "")
    local dir, filename = mputils.split_path(path)
-   print("filename is " .. filename)
    if global_dir == "." then
       global_dir = "./"
    end
@@ -36,7 +35,6 @@ function load_file_handler (event)
       save_file_handler({})
       global_dir = dir
       
-      print(global_dir..DATASTORE_FILENAME)
       tempfile = persistence.load(global_dir..DATASTORE_FILENAME)
       datastore_input = {}
       if tempfile then
@@ -48,36 +46,43 @@ end
 
 function update_current_database_object ()
    local stripped_str = strip_string(global_filename)
-   print("Stripped str is " .. stripped_str)
    local number = get_numbers_from_str(stripped_str)
    local curr_percent_into = mp.get_property("percent-pos","")
    local curr_time_into = mp.get_property("time-pos","")
 
-   local episode_name = get_non_numbers_from_str(stripped_str)
+   global_episode_name = get_non_numbers_from_str(stripped_str)
 
    local original_episode_map = {}
-   if datastore_input[episode_name] ~= nil then
-      original_episode_map = datastore_input[episode_name]["episode_map"]
+   if datastore_input[global_episode_name] ~= nil then
+      original_episode_map = datastore_input[global_episode_name]["episode_map"]
    end
 
-   datastore_input[episode_name]
+   datastore_input[global_episode_name]
       = { ["most_recent_opened_episode_number"] = number,
          ["current_time"] = curr_time_into,
          ["percent_completed"] = curr_percent_into,
          ["episode_map"] = original_episode_map
         }
-   datastore_input[episode_name]["episode_map"][number] = curr_percent_into .. "%"
+   datastore_input[global_episode_name]["episode_map"][number] = curr_percent_into .. "%"
+   global_current_number = number
 end
 
 function save_file_handler (event)
    write_data_file(datastore_input,global_dir,DATASTORE_FILENAME)
 end
 
+function end_load_file_handler()
+   local orig_number = global_current_number
+   load_file_handler()
+   datastore_input[global_episode_name]["episode_map"][orig_number] = "100%"
+end
+   
+
 --datastore_input.gundam = datastore_input.gundam+1
 
 mp.register_event("file-loaded", load_file_handler)
 mp.register_event("shutdown", save_file_handler)
-
+mp.register_event("end-file",end_load_file_handler)
 
 
 
