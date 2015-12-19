536731970914426ae6c970bfb928201f80d04610
banana <bananarangang@gmail.com>
2015-12-19 05:04:24 -0500

    Removed two uneeded globals

--- last_watched.lua
+++ last_watched.lua
@@ -4,8 +4,6 @@ DATASTORE_FILENAME = ".LAST_WATCHED_MPV.MPVWTC"
 datastore_input = persistence.load(DATASTORE_FILENAME) or {}
 global_dir =  "./"
 global_filename = ""
-global_current_number = ""
-global_episode_name = ""
 
 function simple_print_table(table)
    for key,value in pairs(table) do
@@ -54,21 +52,20 @@ function update_current_database_object ()
    local curr_percent_into = mp.get_property("percent-pos","")
    local curr_time_into = mp.get_property("time-pos","")
 
-   global_episode_name = get_non_numbers_from_str(stripped_str)
+   local episode_name = get_non_numbers_from_str(stripped_str)
 
    local original_episode_map = {}
-   if datastore_input[global_episode_name] ~= nil then
-      original_episode_map = datastore_input[global_episode_name]["episode_map"]
+   if datastore_input[episode_name] ~= nil then
+      original_episode_map = datastore_input[episode_name]["episode_map"]
    end
 
-   datastore_input[global_episode_name]
+   datastore_input[episode_name]
       = { ["most_recent_opened_episode_number"] = number,
          ["current_time"] = curr_time_into,
          ["percent_completed"] = curr_percent_into,
          ["episode_map"] = original_episode_map
         }
-   datastore_input[global_episode_name]["episode_map"][number] = curr_percent_into .. "%"
-   global_current_number = number
+   datastore_input[episode_name]["episode_map"][number] = curr_percent_into .. "%"
 end
 
 function save_file_handler (event)
